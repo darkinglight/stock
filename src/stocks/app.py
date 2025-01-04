a84f8133d3b2261f8899e26b5ea98a85ba6e0dd1
Janet <janet@Janets-MacBook-Pro.local>
2025-01-04 13:11:27 +0800

    stocklist

--- src/stocks/app.py
+++ src/stocks/app.py
@@ -4,48 +4,36 @@ stock filter application
 
 import toga
 from stocks.stocklist import stock_list
-from toga.style import Pack
-from toga.style.pack import COLUMN, ROW, CENTER, BOTTOM
 
 
 class stock(toga.App):
-    box = toga.Box(style=Pack(direction=COLUMN, alignment=BOTTOM, padding=10))
-
     def startup(self):
-        main_box = toga.Box()
-        main_box.add(self.nav())
-        main_box.add(self.box)
-
         self.main_window = toga.MainWindow(title=self.formal_name)
-        self.main_window.content = main_box
+        self.main_window.content = toga.TextInput(value="welcome")
+        self.menu()
         self.main_window.show()
 
-    def nav(self):
-        def page_one(widget):
-            self.box.clear()
-            self.box.add(stock_list())
+    def menu(self):
+        def action(widget):
+            self.main_window.content = stock_list()
 
-        def page_two(widget):
-            self.box.clear()
-            self.box.add(toga.TextInput(value='page two.'))
+        things = toga.Group("Things")
+        cmd0 = toga.Command(
+            action,
+            text="港股",
+            tooltip="港股排名",
+            icon="resources/icons/brutus",
+            group=things,
+        )
+        cmd1 = toga.Command(
+            action,
+            text="A股",
+            tooltip="A股排名",
+            icon="resources/icons/cricket-72.png",
+            group=things,
+        )
 
-        def page_three(widget):
-            self.box.clear()
-            self.box.add(toga.TextInput(value='page three.'))
-
-        nav_box = toga.Box(style=Pack(direction=ROW, alignment=CENTER, padding=(0, 10)))
-        icon_style = Pack(width=24)
-        nav_button_one = toga.Button('🏠', on_press=page_one, style=icon_style)
-        nav_box.add(nav_button_one)
-        nav_button_two = toga.Button('📚', on_press=page_two, style=icon_style)
-        nav_box.add(nav_button_two)
-        nav_button_three = toga.Button('🛠️', on_press=page_three, style=icon_style)
-        nav_box.add(nav_button_three)
-        return nav_box
-
-
-
-
+        self.main_window.toolbar.add(cmd0, cmd1)
 
 
 def main():
