a5bae306ecbf9f2f33abc05b3440ba68c1c4b370
chenyuejun <chenyuejun@eastmoney.com>
2025-01-14 17:31:44 +0800

    mm

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,4 @@
 import akshare as ak
 
-stock_info_a_code_name_df = ak.stock_info_a_code_name()
-print(stock_info_a_code_name_df)
+stock_fhps_em_df = ak.stock_fhps_em(date="20231231")
+print(stock_fhps_em_df)
