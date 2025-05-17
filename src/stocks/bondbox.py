b2e19d13c22ccd7073e7a7bcea673ced682081f2
Janet <janet@Janets-MacBook-Pro.local>
2025-05-17 20:58:47 +0800

    modify

--- src/stocks/bondbox.py
+++ src/stocks/bondbox.py
@@ -26,17 +26,20 @@ class BondBox(toga.Box):
                 bond_price = 1000
             if bond_price > 150:
                 continue
-
             stock_detail = hs_detail_repository.fetch_one_from_db(row.stock_code)
+            if stock_detail is None or stock_detail.roe_ttm < 5 or stock_detail.debt_ratio > 60:
+                continue
             box_data.append((
                 row.bond_code,
                 row.bond_name,
                 row.bond_price,
                 row.bond_over_percent,
-                row.stock_over_percent
+                row.stock_over_percent,
+                stock_detail.roe_ttm,
+                stock_detail.debt_ratio
             ))
         box_data.sort(reverse=False, key=lambda a: a[2])
-        return toga.Table(headings=["转债代码", "转债名称", "转债最新价", "纯债溢价率", "转股溢价率"],
+        return toga.Table(headings=["转债代码", "转债名称", "转债最新价", "纯债溢价率", "转股溢价率", "年化净资产收益率(%)", "资产负债率(%)"],
                           data=box_data,
                           on_select=on_active,
                           style=Pack(flex=1))
