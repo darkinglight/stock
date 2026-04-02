bb9de326fdf1b920331d7eb4adfc6833a1a5d569
chenyuejun <chenyuejun@eastmoney.com>
2026-04-02 11:04:27 +0800

    mm

--- stocks/src/stocks/app.py
+++ stocks/src/stocks/app.py
@@ -5,53 +5,22 @@ My first application
 import toga
 from toga.style.pack import COLUMN, ROW, Pack
 
-from services.a_stock_service import AStockService
-from view.stock_list import StockListView
-from database.connection import DatabaseConnectionManager
+from stocks.a_stock_controller import AStockController
 
 
 class stocks(toga.App):
     def startup(self):
-        db_manager = DatabaseConnectionManager()
-        db_manager.set_default_db_name(r"D:\Site\stock\finance.db")
+        self.controller = AStockController()
 
         main_box = toga.Box(style=toga.style.Pack(flex=1, direction=COLUMN))
 
-        service = AStockService()
-        stocks_data = service.get_stocks_paginated(page=1, page_size=20, sort_by='growth', sort_order='desc')
-        service.close()
-
-        self.stock_list_view = StockListView(stocks=stocks_data, on_config_change=self._on_config_change)
+        self.stock_list_view = self.controller.initialize_stock_list()
         main_box.add(self.stock_list_view)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
-        self._setup_toolbar()
+        self.controller.setup_toolbar(self.main_window)
         self.main_window.show()
-
-    def _setup_toolbar(self):
-        cmd_config = toga.Command(
-            action=self._show_config,
-            text="配置",
-            tooltip="列表配置",
-            icon="resources/config.png"
-        )
-        self.main_window.toolbar.add(cmd_config)
-
-    def _show_config(self, widget):
-        self.stock_list_view.show_config_dialog()
-
-    def _on_config_change(self, config):
-        service = AStockService()
-        stocks_data = service.get_stocks_paginated(
-            page=1,
-            page_size=config['page_size'],
-            sort_by=config['sort_by'],
-            sort_order=config['sort_order'],
-            max_debt_ratio=config['max_debt_ratio']
-        )
-        service.close()
-        self.stock_list_view.update_data(stocks_data)
 
 
 def main():
